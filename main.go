6fe2c1f507259837731a76128cdfe3e66a9bbd7c
SarimSiddd <sarimsiddiq@gmail.com>
2024-12-18 19:39:46 +0400

    [feat] connection to database

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"knowledge-base-assistant/config"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"gopkg.in/yaml.v3"
 )
 
@@ -15,17 +17,32 @@ type T struct {
 
 func main() {
 
-	data, err := os.ReadFile("config/langchain.yml")
+	data, err := os.ReadFile("config/db.yml")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
 
-	var lconfig config.LangChain
-	err = yaml.Unmarshal(data, &lconfig)
+	var dbconfig config.Database
+	err = yaml.Unmarshal(data, &dbconfig)
 	if err != nil {
 		fmt.Println("Error unmarshalling YAML file", err)
 	}
 
-	fmt.Println(lconfig)
+	conn, err := pgx.Connect(context.Background(), "user=sarim password=1234 host=localhost port=3399 dbname=vectors")
+
+	if err != nil {
+		fmt.Println("Error connection to database", err)
+	}
+
+	defer conn.Close(context.Background())
+
+	var res int64
+	err = conn.QueryRow(context.Background(), "SELECT 1 AS res").Scan(&res)
+
+	if err != nil {
+		fmt.Println("Unable to execute query on db", err)
+	}
+
+	fmt.Println("Printing row result: ", res)
 }
