9c6c12f8260456bed714e3ef914455f3df2e105c
SarimSiddd <sarimsiddiq@gmail.com>
2024-12-25 13:15:03 +0400

    Chromadb integration

--- go.mod
+++ go.mod
@@ -2,16 +2,21 @@ module knowledge-base-assistant
 
 go 1.23.0
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	github.com/amikos-tech/chroma-go v0.1.4
+	github.com/jackc/pgx/v5 v5.7.1
+	github.com/tmc/langchaingo v0.1.12
+	gopkg.in/yaml.v3 v3.0.1
+)
 
 require (
+	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
-	github.com/tmc/langchaingo v0.1.12 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"knowledge-base-assistant/config"
 	"log"
 	"os"
+	"path/filepath"
 
+	chroma "github.com/amikos-tech/chroma-go"
+	huggingface "github.com/amikos-tech/chroma-go/hf"
+	"github.com/amikos-tech/chroma-go/types"
 	"github.com/jackc/pgx/v5"
-	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/anthropic"
 	"gopkg.in/yaml.v3"
 )
@@ -50,13 +54,61 @@ func main() {
 		log.Fatal(err)
 	}
 
-	prompt := "What is the capital of France?"
-	completion, err := llms.GenerateFromSinglePrompt(context.Background(), llm, prompt)
+	_ = llm
+
+	dir := "./samples/"
+
+	embedder := huggingface.NewHuggingFaceEmbeddingFunction(os.Getenv("HUGGINGFACEHUB_API_TOKEN"), "sentence-transformers/all-MiniLM-L6-v2")
+
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			// Read the file content
+			content, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			// Process the file content
+			result, err := embedder.EmbedQuery(context.Background(), string(content))
+
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Embedding result for file %s: %v\n", path, result)
+		}
+
+		return nil
+	})
+
+	result, err := embedder.EmbedQuery(context.Background(), "Hello World")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(completion)
+	client, err := chroma.NewClient("http://localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection, err := client.CreateCollection(context.Background(), "test-collection", map[string]interface{}{"key": "value"}, true, embedder, types.L2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Collection created: ", collection)
+
+	collection_result, err := collection.Add(context.Background(), []*types.Embedding{result}, []map[string]interface{}{}, []string{"Hello World"}, []string{"doc1"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Collection result: ", collection_result)
 
 }
